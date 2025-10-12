9c74b3086896254c662b1681016bac8b366ecb90
Manveer Kaur <manveer09bajwa@email.com>
2025-10-12 14:05:26 -0400

    second commit

---
+++ conditional statement/oddeven.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter a number:";
+    cin>>x;
+    if(x%2==0){
+       cout<<"even number";
+    }
+    else{
+        cout<<"odd number";
+    }
+}
