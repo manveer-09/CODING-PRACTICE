af2ba46a7ef657604c8e073a4323a78183432ee2
Manveer kaur <manveer09bajwa@email.com>
2025-10-20 09:07:35 -0400

    changed

--- Loops/EvenNumberFrom1to100.cpp
+++ Loops/EvenNumberFrom1to100.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"enter a number :";
+   // cout<<"enter a number :";
     // for(int i=1; i<=100;i++){
     //     if(i%2==0)
     //     cout<<i<<" ";
