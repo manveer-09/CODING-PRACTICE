9bf651d70f09ab05a760889ecf930c14e851e6ee
Manveer Kaur <manveer09bajwa@gmail.com>
2025-12-25 04:16:47 -0500

    New program added

---
+++ OOPS/OperatorOverloading.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class Complex{
+    int real,img;
+    public:
+
+    Complex(){
+
+    }
+
+    Complex(int real,int img){
+        this->real = real;
+        this->img = img;
+    }
+
+    void display(){
+        cout<<real<<" + "<<img<<"i" <<endl;
+    }
+    Complex operator +(Complex &C){
+        Complex ans;
+        ans.real = real+C.real;
+        ans.img = img +C.img;
+        return ans;
+
+    }
+};
+    int main(){
+    Complex C1(3,4);
+    Complex C2(5,4);
+    Complex C3 = C1+C2; // C1.fun(C2)
+    C3.display();
+}
