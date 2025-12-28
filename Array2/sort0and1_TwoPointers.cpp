c4f8ddd2cd3728d608c696d221d0a2c6be9e692b
Manveer Kaur <manveer09bajwa@gmail.com>
2025-12-28 07:40:27 +0530

    sort 0 and 1 -Two Pointer

---
+++ Array2/sort0and1_TwoPointers.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void sort0and1(int arr[],int n){
+   int i=0;
+   int j=n-1;
+    
+   while(i<j){
+    if(arr[i]==0)   i++;
+    else if(arr[j]==1)  j--;
+    else{
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        i++;
+        j--;
+    }
+   }
+
+}
+int main(){
+    int n;
+    cout<<"Enter size of array:";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the element of array:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    sort0and1(arr,n);
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    
+}
