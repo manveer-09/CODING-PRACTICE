3f0ca1d3a03ccfbf3104b3e23b5b113dd9930037
Manveer Kaur <manveer09bajwa@gmail.com>
2026-01-02 05:14:11 +0530

    intro of 2d array

---
+++ 2dArray1/input.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter the no of rows :";
+    cin>>m;
+    int n;
+    cout<<"Enter the no of columns :";
+    cin>>n;
+    int arr[m][n];
+    //input
+    for(int i=0;i<=m-1;i++){
+        for(int j=0;j<=n-1;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    //output
+    for(int i=0;i<=m-1;i++){
+        for(int j=0;j<=n-1;j++){
+            cout<<arr[i][j]<<" ";
+        }
+    cout<<endl;
+    }
+
+}
