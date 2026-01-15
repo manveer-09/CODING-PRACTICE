b67876698f8dd835483a5d845bd3ee5f67900dba
Manveer Kaur <manveer09bajwa@gmail.com>
2026-01-15 04:24:10 -0800

    use stoi/stoll for string to integer conversion

---
+++ String1/stoi_stoll.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    // string s="12346788876";
+    // int x=stoi(s);
+    // cout<<x;
+
+    string s="1343566786643424";
+    long long x =stoll(s);
+    cout<<x;
+
+}
